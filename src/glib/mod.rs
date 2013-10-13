16592a7bc5f50246baaeb355e3ce896192508b8a
Cadence Marseille <cadencemarseille@gmail.com>
2013-10-13 10:01:41 -0400

    Add `glibconfig.h` types

--- src/glib/mod.rs
+++ src/glib/mod.rs
@@ -9,6 +9,23 @@
 #[link(name = "glib", vers = "0.1")];
 #[crate_type = "lib"];
 
+use std::libc;
+
 pub use types::*;
 
 pub mod types;
+
+pub type gint8 = i8;
+pub type guint8 = u8;
+pub type gint16 = i16;
+pub type guint16 = u16;
+pub type gint32 = i32;
+pub type guint32 = u32;
+pub type gint64 = i64;
+pub type guint64 = u64;
+
+pub type gssize = libc::ssize_t;
+pub type gsize = libc::size_t;
+
+pub type gintptr = libc::intptr_t;
+pub type guintptr = libc::uintptr_t;
